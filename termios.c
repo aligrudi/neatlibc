d6efb0c57640a46a89a7769372867de38376f364
Ali Gholami Rudi <ali@rudi.ir>
2011-05-20 11:08:33 +0430

    termios: add cfmakeraw()

--- termios.c
+++ termios.c
@@ -20,3 +20,15 @@ int tcsetattr(int fd, int actions, struct termios* term)
 	errno = EINVAL;
 	return -1;
 }
+
+void cfmakeraw(struct termios *t)
+{
+	t->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP |
+			INLCR | IGNCR | ICRNL | IXON);
+	t->c_oflag &= ~OPOST;
+	t->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	t->c_cflag &= ~(CSIZE | PARENB);
+	t->c_cflag |= CS8;
+	t->c_cc[VMIN] = 1;
+	t->c_cc[VTIME] = 0;
+}
