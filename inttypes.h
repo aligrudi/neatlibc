c7b18d93dd62a6c431958adc579f7e7940bfbbd3
Ali Gholami Rudi <ali@rudi.ir>
2011-05-24 12:33:53 +0430

    stdint.h: added

--- inttypes.h
+++ inttypes.h
@@ -1,11 +1,1 @@
-#ifndef _INTTYPES_H
-#define _INTTYPES_H
-
-typedef char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-
-#endif
+#include <stdint.h>
