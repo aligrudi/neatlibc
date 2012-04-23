77d41615e142c13142862de1a24d91619c745e8f
Ali Gholami Rudi <ali@rudi.ir>
2012-04-23 18:24:53 +0430

    stddef.h: size_t should be unsigned long

--- stddef.h
+++ stddef.h
@@ -1,9 +1,9 @@
-#ifndef _STDLIB_H
-#define _STDLIB_H
+#ifndef _STDDEF_H
+#define _STDDEF_H
 
 #define NULL			((void *) 0)
 #define offsetof(type, field)	((int) (&((type *) 0)->field))
 
-typedef unsigned size_t;
+typedef unsigned long size_t;
 
 #endif
