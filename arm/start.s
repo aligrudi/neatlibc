1504b1eb2bfebad823ab31fb49b8aca662cfc9e9
Ali Gholami Rudi <ali@rudi.ir>
2014-12-02 16:44:36 +0330

    start: atexit() for arm

--- arm/start.s
+++ arm/start.s
@@ -1,5 +1,6 @@
 .extern environ
 .extern main
+.extern __neatlibc_exit
 
 .global _start
 _start:
@@ -12,6 +13,9 @@ _start:
 	str	a3, [r12]
 
 	bl	main
+	mov	r4, r0
+	bl	__neatlibc_exit
+	mov	r0, r4
 
 	mov	r7, #1
 	swi	#0
