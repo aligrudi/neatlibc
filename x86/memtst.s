de69c3399ff2c8ad412d06ebff7cfeac8ff48c90
Ali Gholami Rudi <ali@rudi.ir>
2014-12-21 17:05:40 +0330

    memtst: support for x86

---
+++ x86/memtst.s
@@ -0,0 +1,17 @@
+format ELF
+
+public memtst_back
+memtst_back:
+	mov	eax, ebp
+	mov	ecx, [esp+4]
+bt_up:	cmp	ecx, 0
+	jle	bt_out
+	test	eax, eax
+	jz	bt_out
+	mov	eax, [eax]
+	dec	ecx
+	jmp	bt_up
+bt_out:	test	eax, eax
+	jz	bt_ret
+	mov	eax, [eax+4]
+bt_ret:	ret
