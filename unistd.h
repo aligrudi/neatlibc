ecef53ad06906a0535ab435bb812a45ea9659bd2
Ali Gholami Rudi <ali@rudi.ir>
2014-12-26 19:02:48 +0330

    unistd.h: declare pipe() and dup()

--- unistd.h
+++ unistd.h
@@ -1,17 +1,11 @@
 #include <stddef.h>
 
-#define SEEK_SET	0
-#define SEEK_CUR	1
-#define SEEK_END	2
-
-#define STDIN_FILENO	0
-#define STDOUT_FILENO	1
-#define STDERR_FILENO	2
-
+/* access() flags */
 #define R_OK		4
 #define W_OK		2
 #define X_OK		1
 #define F_OK		0
+
 int access(char *name, int type);
 int unlink(char *path);
 
@@ -21,17 +15,31 @@ int close(int fd);
 long write(int fd, void *buf, long n);
 long read(int fd, void *buf, long n);
 
+/* lseek() whence */
 #define SEEK_SET	0
 #define SEEK_CUR	1
 #define SEEK_END	2
+
 long lseek(int fd, long offset, int whence);
 
-void _exit(int status);
+int pipe(int fds[2]);
+int dup(int fd);
+int dup2(int fd, int fd2);
+
 int fork(void);
 int getpid(void);
 int getppid(void);
+
 int execve(char *path, char *argv[], char *envp[]);
 int execle(char *path, ...);
 int execvp(char *file, char *argv[]);
 int execv(char *path, char *argv[]);
+
+void _exit(int status);
+
 int sleep(int n);
+
+/* standard file descriptors */
+#define STDIN_FILENO	0
+#define STDOUT_FILENO	1
+#define STDERR_FILENO	2
